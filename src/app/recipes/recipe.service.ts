f36258724a7f5e9d046da8994a0f0a28f9a337c0
Soufiene OUERTANI <soufiene.ouertani@gmail.com>
2024-02-16 19:51:34 +0100

    125. Adding and displaying Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,27 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe1', 'This is simply a test1', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('A Test Recipe2', 'This is simply a test2', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe(
+          'A Test Recipe1', 
+          'This is simply a test1', 
+          'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', 
+          [
+            new Ingredient("Meat", 1),
+            new Ingredient("Frenc freis", 2)
+          ]),
+        new Recipe(
+          'A Test Recipe2', 
+          'This is simply a test2', 
+          'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', 
+          [
+            new Ingredient("Buns", 2),
+            new Ingredient("Meat", 5)
+         ])
       ];
 
       getRecipes(){
